26b22f240a01025ca613b9e7547d8d681c1a0c3a
Juan Pedro Bretti Mandarano <juan+github@bretti.com.ar>
2021-02-19 09:06:16 +0100

    Clean up

--- 2_kafka_to_spark.py
+++ 2_kafka_to_spark.py
@@ -1,16 +1,12 @@
 from pyspark import SparkConf, SparkContext
 from pyspark.streaming import StreamingContext
-from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from pyspark.sql.session import SparkSession
 from pyspark.streaming.kafka import KafkaUtils
-import pandas as pd
 import sys
-from textblob import TextBlob
 from constants import *
 
 # Defines if a tweet is positive, neutral or negative
-# https://stackoverflow.com/a/48874376/3780957
 def check_positive_negative(tweet):
     tweet = tweet.split(' ')
     positive = len([w for w in tweet if w in words_positive])
@@ -34,21 +30,21 @@ sc.setLogLevel("ERROR")
 
 # create the Streaming Context from the above spark context with interval size 2 seconds
 # Only one StreamingContext can be active in a JVM at the same time. (https://spark.apache.org/docs/2.0.0/streaming-programming-guide.html)
-ssc = StreamingContext(sc, 2)
+sc_sc = StreamingContext(sc, 2)
 
 # setting a checkpoint to allow RDD recovery
 # TODO: setting a checkpoint to allow RDD recovery
-ssc.checkpoint("checkpoint_TwitterApp")
+sc_sc.checkpoint("checkpoint_TwitterApp")
 
 # Read word for sentiment analysis
-spark = SparkSession(sc)
+ss_sc = SparkSession(sc)
 # Converted to Python lists
 # https://stackoverflow.com/a/64764406/3780957
-words_positive = spark.read.option("header", "false").csv("words/positive-words.txt").select('_c0').rdd.flatMap(list).collect()
-words_negative = spark.read.option("header", "false").csv("words/negative-words.txt").select('_c0').rdd.flatMap(list).collect()
+words_positive = ss_sc.read.option("header", "false").csv("words/positive-words.txt").select('_c0').rdd.flatMap(list).collect()
+words_negative = ss_sc.read.option("header", "false").csv("words/negative-words.txt").select('_c0').rdd.flatMap(list).collect()
 
 # read data from the port
-dataStream = KafkaUtils.createStream(ssc, KAFKA_ZOOKEEPER_SERVERS, 'streaming-consumer', {KAFKA_TOPIC: 1})
+dataStream = KafkaUtils.createStream(sc_sc, KAFKA_ZOOKEEPER_SERVERS, 'streaming-consumer', {KAFKA_TOPIC: 1})
 # No need to decode() from 'utf-8' in Python3
 dataStream = dataStream.map(lambda x: x[1].lower())
 # Split each single tweet. The "end of tweet" was set at the Twitter pull application
@@ -66,8 +62,8 @@ sorted_ = tweets_totals.transform(lambda rdd: rdd.sortBy(lambda x: x[1], ascendi
 sorted_.pprint()
 
 # start the streaming computation
-ssc.start()
+sc_sc.start()
 
 # wait for the streaming to finish
-ssc.awaitTermination()
+sc_sc.awaitTermination()
 
